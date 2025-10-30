f554b959d7c3a01601963306a6c1cd90250dea31
Sinity <ezo.dev@gmail.com>
2025-10-30 18:14:05 +0100

    Simplify flake and enforce CLI dependencies

--- flake.nix
+++ flake.nix
@@ -8,86 +8,96 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
+      inherit (flake-utils.lib) eachDefaultSystem;
+
+      re2Overlay = final: prev: {
+        re2 = prev.re2.overrideAttrs (_: {
+          src = prev.fetchFromGitHub {
+            owner = "google";
+            repo = "re2";
+            rev = "ac82d4f628a2045d89964ae11c48403d3b091af1";
+            hash = "sha256-qRNV0O55L+r2rNSUJjU6nMqkPWXENZQvyr5riTU3e5o=";
+          };
+        });
+      };
+
       polylogueModule = import ./nix/modules/polylogue.nix { inherit self; };
     in
-    flake-utils.lib.eachDefaultSystem (system:
+    eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            (final: prev: {
-              re2 = prev.re2.overrideAttrs (old: {
-                src = prev.fetchFromGitHub {
-                  owner = "google";
-                  repo = "re2";
-                  rev = "ac82d4f628a2045d89964ae11c48403d3b091af1";
-                  hash = "sha256-qRNV0O55L+r2rNSUJjU6nMqkPWXENZQvyr5riTU3e5o=";
-                };
-              });
-            })
-          ];
+          overlays = [ re2Overlay ];
         };
-        python = pkgs.python3;
+
         pyPkgs = pkgs.python3Packages;
-        baseDeps =
-          with pyPkgs;
-          [
-            google-auth-oauthlib
-            requests
-            pathvalidate
-            aiohttp
-            aiofiles
-            rich
-            pydantic
-            python-frontmatter
-            jinja2
-            markdown-it-py
-            pyperclip
-            watchfiles
-            tiktoken
-            qdrant-client
-          ];
+
+        baseDeps = with pyPkgs; [
+          google-auth-oauthlib
+          requests
+          pathvalidate
+          aiohttp
+          aiofiles
+          rich
+          pydantic
+          python-frontmatter
+          jinja2
+          markdown-it-py
+          pyperclip
+          watchfiles
+          tiktoken
+          qdrant-client
+        ];
+
+        cliDeps = with pkgs; [ gum skim delta git ];
+        cliBinPath = pkgs.lib.makeBinPath cliDeps;
+
         polylogueApp = pyPkgs.buildPythonApplication {
           pname = "polylogue";
           version = "0.1.0";
           pyproject = true;
           src = self;
           propagatedBuildInputs = baseDeps;
-          nativeBuildInputs = with pyPkgs; [
-            setuptools
-            wheel
-          ];
-          nativeCheckInputs =
-            (with pyPkgs; [ pytest ])
-            ++ (with pkgs; [ delta gum git ]);
+          nativeBuildInputs =
+            (with pyPkgs; [ setuptools wheel ])
+            ++ cliDeps
+            ++ [ pkgs.makeWrapper ];
+          nativeCheckInputs = (with pyPkgs; [ pytest ]) ++ cliDeps;
           checkPhase = "pytest";
+          postInstall = ''
+            wrapProgram $out/bin/polylogue \
+              --prefix PATH : ${cliBinPath}
+          '';
         };
+
+        defaultDevShell = import ./nix/devshell.nix {
+          inherit pkgs;
+          extraPythonPackages = with pyPkgs; [
+            pytest
+            pytest-cov
+            coverage
+            mypy
+            types-requests
+          ];
+        };
+
         cliApp = {
           type = "app";
           program = "${polylogueApp}/bin/polylogue";
         };
       in {
         packages = {
-          default = polylogueApp;
           polylogue = polylogueApp;
+          default = polylogueApp;
         };
 
         apps = {
-          default = cliApp;
           polylogue = cliApp;
+          default = cliApp;
         };
 
         devShells = {
-          default = import ./nix/devshell.nix {
-            inherit pkgs;
-            extraPythonPackages = with pkgs.python3Packages; [
-              pytest
-              pytest-cov
-              coverage
-              mypy
-              types-requests
-            ];
-          };
+          default = defaultDevShell;
           ci = pkgs.mkShell {
             buildInputs = [ polylogueApp pkgs.git pkgs.which ];
             shellHook = ''
@@ -97,9 +107,7 @@
           };
         };
 
-        checks = {
-          default = polylogueApp;
-        };
+        checks.default = polylogueApp;
       }
     ) // {
       nixosModules = {
