55bdd8074da02a83dd576c5febba9df41bd0a456
Sinity <ezo.dev@gmail.com>
2026-04-12 14:00:07 +0200

    fix: restore search read-connection boundaries

--- polylogue/storage/search.py
+++ polylogue/storage/search.py
@@ -1,7 +1,7 @@
 """Public FTS5 search surface."""
 
 from polylogue.storage import search_runtime as _search_runtime
-from polylogue.storage.backends.connection import open_connection
+from polylogue.storage.backends.connection import open_read_connection as open_connection
 from polylogue.storage.search_models import SearchHit, SearchResult
 from polylogue.storage.search_query_builders import (
     build_ranked_action_search_query,
@@ -12,7 +12,7 @@ from polylogue.storage.search_query_support import _FTS5_SPECIAL, escape_fts5_qu
 
 def search_messages(*args, **kwargs):
     """Search for messages using the patch-safe runtime implementation."""
-    _search_runtime.open_connection = open_connection
+    _search_runtime.open_read_connection = open_connection
     return _search_runtime.search_messages(*args, **kwargs)
 
 
